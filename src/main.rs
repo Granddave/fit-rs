b563f98780051323916dd75f768b2831e11d5a94
David Isaksson <davidisaksson93@gmail.com>
2023-10-29 21:17:09 +0100

    Add beginning of Hilbert curve visualization

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,48 @@ use anyhow::{anyhow, Context, Result};
 
 const MAP_SIZE: usize = 256;
 
+mod hilbert {
+    use super::MAP_SIZE;
+
+    fn rot(n: i32, x: &mut i32, y: &mut i32, rx: i32, ry: i32) {
+        if ry == 0 {
+            if rx == 1 {
+                *x = n - 1 - *x;
+                *y = n - 1 - *y;
+            }
+
+            std::mem::swap(x, y);
+        }
+    }
+
+    fn d2xy(total: i32, distance: i32) -> (i32, i32) {
+        let mut x = 0;
+        let mut y = 0;
+        let mut t = distance;
+        let mut s = 1;
+
+        while s < total {
+            let rx = 1 & (t / 2);
+            let ry = 1 & (t ^ rx);
+            rot(s, &mut x, &mut y, rx, ry);
+
+            x += s * rx;
+            y += s * ry;
+            t /= 4;
+            s *= 2;
+        }
+
+        (x, y)
+    }
+
+    pub fn linear(bytes: &[u8], pixels: &mut [u8]) {
+        for (ix, byte) in bytes.iter().enumerate() {
+            let (x, y) = d2xy(MAP_SIZE as i32, ix as i32);
+            pixels[y as usize * MAP_SIZE + x as usize] = *byte;
+        }
+    }
+}
+
 mod digraph {
     use super::MAP_SIZE;
     pub fn linear(bytes: &[u8], pixels: &mut [u8]) {
@@ -66,6 +108,7 @@ mod digraph {
 enum Visualization {
     DigraphLinear,
     DigraphLog,
+    Hilbert,
 }
 
 fn main() -> Result<()> {
@@ -84,6 +127,7 @@ fn main() -> Result<()> {
     match vis {
         Visualization::DigraphLinear => digraph::linear(&bytes, &mut pixels),
         Visualization::DigraphLog => digraph::log(&bytes, &mut pixels),
+        Visualization::Hilbert => hilbert::linear(&bytes, &mut pixels),
     }
 
     image::save_buffer(
