11279b13aedaaa4845e67c8fb1bdd0725bd712ee
David Isaksson <davidisaksson93@gmail.com>
2023-11-02 22:07:32 +0100

    Render visualization algorithms and add keyboard interactions

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,15 @@ pub const MAP_SIZE: usize = 256;
 pub mod digraph;
 pub mod hilbert;
 
+#[derive(Clone, Copy, Debug)]
 pub enum Visualization {
     DigraphLinear,
     DigraphLog,
     Hilbert,
+}
+
+impl Default for Visualization {
+    fn default() -> Self {
+        Visualization::DigraphLog
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,55 +2,7 @@ use eframe::egui;
 use egui::*;
 use std::cmp::{max, min};
 
-use anyhow::{anyhow, Context, Result};
 use fit_rs::{digraph, hilbert, Visualization, MAP_SIZE};
-
-// fn save_image(pixels: &[u8], filename: &str) -> Result<()> {
-//     image::save_buffer(
-//         filename,
-//         &pixels,
-//         MAP_SIZE as u32,
-//         MAP_SIZE as u32,
-//         image::ColorType::L8,
-//     )
-//     .context("save image")
-// }
-
-// fn main() -> Result<()> {
-//     // TODO: use clap
-//     let filepath = std::env::args().nth(1).context("read filename")?;
-//     let bytes = std::fs::read(filepath).context("read file")?;
-//
-//     // Store the second cli argument as an int
-//     let offset: usize = std::env::args()
-//         .nth(2)
-//         .unwrap_or("0".to_string())
-//         .parse()
-//         .context("parse offset")?;
-//
-//     if bytes.len() < 2 {
-//         return Err(anyhow!("File is too short"));
-//     }
-//
-//     let mut pixels = [0u8; MAP_SIZE * MAP_SIZE];
-//
-//     let slice_begin: usize = 0 + offset;
-//     let slice_end: usize = min(
-//         max(bytes.len(), MAP_SIZE * MAP_SIZE),
-//         (MAP_SIZE * MAP_SIZE) + offset,
-//     );
-//
-//     let vis = Visualization::Hilbert;
-//     match vis {
-//         Visualization::DigraphLinear => digraph::linear(&bytes, &mut pixels),
-//         Visualization::DigraphLog => digraph::log(&bytes, &mut pixels),
-//         Visualization::Hilbert => hilbert::linear(&bytes[slice_begin..slice_end], &mut pixels),
-//     }
-//
-//     save_image(&pixels, &format!("image_{:0>6}.png", offset))?;
-//
-//     Ok(())
-// }
 
 fn main() -> Result<(), eframe::Error> {
     // env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
@@ -62,50 +14,151 @@ fn main() -> Result<(), eframe::Error> {
     )
 }
 
-#[derive(Default)]
 struct Content {
-    size: usize,
+    zoom: usize,
     bytes: Vec<u8>,
-    pixels: Vec<u8>, //u8; MAP_SIZE * MAP_SIZE],
+    pixels: Vec<u8>,
+    offset: usize,
+    offset_step_size: usize,
+    algoritm: Visualization,
+    slice_begin: usize,
+    slice_end: usize,
+}
+
+// Default for Content
+impl Default for Content {
+    fn default() -> Self {
+        Content {
+            zoom: 1,
+            bytes: Vec::new(),
+            pixels: Vec::new(),
+            offset: 0,
+            offset_step_size: 10,
+            algoritm: Visualization::default(),
+            slice_begin: 0,
+            slice_end: 0,
+        }
+    }
+}
+
+impl Content {
+    fn read_file(&mut self) {
+        let filepath = std::env::args().nth(1).unwrap();
+        self.bytes = std::fs::read(filepath).unwrap();
+        self.pixels = vec![0; MAP_SIZE * MAP_SIZE];
+
+        self.slice_end = min(
+            max(self.bytes.len(), MAP_SIZE * MAP_SIZE),
+            (MAP_SIZE * MAP_SIZE) + self.offset,
+        );
+
+        eprintln!("Pixels size: {}", self.pixels.len());
+        self.calculate();
+    }
+
+    fn draw_map(&mut self, ui: &mut egui::Ui) {
+        if !self.pixels.is_empty() {
+            ui.painter().rect_filled(
+                Rect {
+                    min: pos2(0.0, 0.0),
+                    max: pos2(MAP_SIZE as f32, MAP_SIZE as f32),
+                },
+                0.0,
+                Color32::from_gray(0),
+            );
+            for y in 0..MAP_SIZE {
+                for x in 0..MAP_SIZE {
+                    let value = (self.pixels[y * MAP_SIZE + x]) as u8;
+                    let s = self.zoom;
+                    ui.painter().rect_filled(
+                        Rect {
+                            min: pos2((x * s) as f32, (y * s) as f32),
+                            max: pos2(((x * s) + s) as f32, ((y * s) + s) as f32),
+                        },
+                        0.0,
+                        Color32::from_gray(value),
+                    );
+                }
+            }
+        }
+    }
+
+    fn calculate(&mut self) {
+        if self.bytes.len() < 2 {
+            return;
+        }
+        eprintln!("Algoritm: {:?}", self.algoritm);
+        match self.algoritm {
+            Visualization::DigraphLinear => digraph::linear(&self.bytes, &mut self.pixels),
+            Visualization::DigraphLog => digraph::log(&self.bytes, &mut self.pixels),
+            Visualization::Hilbert => {
+                self.slice_begin = 0 + self.offset;
+                self.slice_end = min(
+                    max(self.bytes.len(), MAP_SIZE * MAP_SIZE),
+                    (MAP_SIZE * MAP_SIZE) + self.offset,
+                );
+                hilbert::linear(
+                    &self.bytes[self.slice_begin..self.slice_end],
+                    &mut self.pixels,
+                )
+            }
+        }
+    }
 }
 
 impl eframe::App for Content {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            if ctx.input(|i| i.key_pressed(Key::L)) {
-                let filepath = std::env::args().nth(1).context("read filename").unwrap();
-                self.bytes = std::fs::read(filepath).context("read file").unwrap();
-                // Initialize pixels to 0 with the size of MAP_SIZE * MAP_SIZE
-                self.pixels = vec![0; MAP_SIZE * MAP_SIZE];
-                digraph::log(&self.bytes, &mut self.pixels)
+            if ctx.input(|i| i.key_pressed(Key::R)) {
+                self.read_file();
             }
 
-            if !self.pixels.is_empty() {
-                for y in 0..MAP_SIZE {
-                    for x in 0..MAP_SIZE {
-                        let value = (self.pixels[y * MAP_SIZE + x]) as u8;
-                        let s = self.size;
-                        ui.painter().rect_filled(
-                            Rect {
-                                min: pos2((x * s) as f32, (y * s) as f32),
-                                max: pos2(((x * s) + s) as f32, ((y * s) + s) as f32),
-                            },
-                            0.0,
-                            Color32::from_gray(value),
-                        );
-                    }
+            self.draw_map(ui);
+
+            if ctx.input(|i| i.key_pressed(Key::H)) {
+                self.algoritm = Visualization::Hilbert;
+                self.calculate();
+            }
+            if ctx.input(|i| i.key_pressed(Key::L)) {
+                self.algoritm = Visualization::DigraphLog;
+                self.calculate();
+            }
+
+            if ctx.input(|i| i.key_pressed(Key::ArrowRight)) {
+                if self.offset + 10 * self.offset_step_size < self.bytes.len() {
+                    self.offset += 10 * self.offset_step_size;
+                    self.calculate();
                 }
             }
-            // if ui.button("Clear").clicked() {
-            //     self.text.clear();
-            // }
+
+            if ctx.input(|i| i.key_pressed(Key::ArrowLeft)) {
+                if self.offset - self.offset_step_size > 0 {
+                    self.offset -= 10 * self.offset_step_size;
+                    self.calculate();
+                }
+            }
+
+            if ctx.input(|i| i.key_pressed(Key::ArrowUp)) {
+                self.offset_step_size += 10;
+            }
+
+            if ctx.input(|i| i.key_pressed(Key::ArrowDown)) {
+                if self.offset_step_size != 0 {
+                    self.offset_step_size -= 10;
+                }
+            }
 
             if ctx.input(|i| i.key_pressed(Key::Q)) {
-                self.size += 1;
+                frame.close();
             }
-            if ctx.input(|i| i.key_pressed(Key::A)) {
-                if self.size > 1 {
-                    self.size -= 1;
+
+            if ctx.input(|i| i.key_pressed(Key::PlusEquals)) {
+                self.zoom += 1;
+            }
+
+            if ctx.input(|i| i.key_pressed(Key::Minus)) {
+                if self.zoom > 1 {
+                    self.zoom -= 1;
                 }
             }
         });
